d807ffbf941ce06c85bc01548711e2efc3401439
mr.loverman <162125173+ToekSopanha@users.noreply.github.com>
2026-03-18 16:19:00 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Request-Absence_bot/'
   plugins: [
     vue(),
     vueDevTools(),
